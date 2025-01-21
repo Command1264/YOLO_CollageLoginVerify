896e0f88d1922a3ea362f0c550210495a6588460
指令Command1 <command1264@gmail.com>
2025-01-21 16:41:12 +0800

    feat: add GoogleSheetCellFormat module for defining Google Sheets cell formatting
    Added the `GoogleSheetCellFormat` module, including the following classes:
    - `SheetNumberFormat`: Handles number format type and pattern.
    - `SheetCellFormat`: Defines cell format details like range, text format, and alignment.
    - `GoogleSheetFormat`: Manages column width and list of cell formats.
    This module provides a structured way to handle Google Sheets cell formatting.

---
+++ collageLogin/GoogleSheetCellFormat.py
@@ -0,0 +1,43 @@
+from typing import Any
+
+from pygsheets import FormatType, HorizontalAlignment
+
+
+class SheetNumberFormat:
+    format_type: FormatType
+    pattern: str
+
+    def __init__(self, format_type: FormatType, pattern: str,):
+        self.format_type = format_type
+        self.pattern = pattern
+
+
+class SheetCellFormat:
+    format_range: str
+    text_format: dict[str, Any]
+    horizontal_alignment: HorizontalAlignment
+    number_format: SheetNumberFormat
+
+    def __init__(
+            self,
+            format_range: str,
+            text_format: dict[str, Any],
+            horizontal_alignment: HorizontalAlignment,
+            number_format: SheetNumberFormat,
+    ):
+        self.format_range = format_range
+        self.text_format = text_format
+        self.horizontal_alignment = horizontal_alignment
+        self.number_format = number_format
+
+class GoogleSheetFormat:
+    column_width: list[int]
+    formats: list[SheetCellFormat]
+
+    def __init__(
+            self,
+            column_width: list[int],
+            formats: list[SheetCellFormat],
+    ):
+        self.column_width = column_width
+        self.formats = formats
